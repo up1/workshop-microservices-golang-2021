c2a1353d5493be545d550631666818070e0d5f68
up1 <somkiat.p@gmail.com>
2021-03-09 15:27:01 +0700

    Working with service discovery

---
+++ sync/working-with-service-discovery/product-service/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	consulapi "github.com/hashicorp/consul/api"
+)
+
+type product struct {
+	ID    uint64  `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+type ProductConfiguration struct {
+	Categories []string `json:"categories"`
+}
+
+func main() {
+	registerServiceWithConsul()
+	http.HandleFunc("/healthcheck", healthcheck)
+	http.HandleFunc("/products", Products)
+	http.HandleFunc("/product-configuration", Configuration)
+	fmt.Printf("product service is up on port: %s", port())
+	http.ListenAndServe(port(), nil)
+}
+
+func registerServiceWithConsul() {
+	config := consulapi.DefaultConfig()
+	consul, err := consulapi.NewClient(config)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	registration := new(consulapi.AgentServiceRegistration)
+
+	registration.ID = "product-service"
+	registration.Name = "product-service"
+	address := hostname()
+	// address := "127.0.0.1"
+	registration.Address = address
+	port, err := strconv.Atoi(port()[1:len(port())])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	registration.Port = port
+	registration.Check = new(consulapi.AgentServiceCheck)
+	registration.Check.HTTP = fmt.Sprintf("http://%s:%v/healthcheck", address, port)
+	registration.Check.Interval = "5s"
+	registration.Check.Timeout = "3s"
+	consul.Agent().ServiceRegister(registration)
+}
+
+func Configuration(w http.ResponseWriter, r *http.Request) {
+	config := consulapi.DefaultConfig()
+	consul, err := consulapi.NewClient(config)
+	if err != nil {
+		fmt.Fprintf(w, "Error. %s", err)
+		return
+	}
+	kvpair, _, err := consul.KV().Get("product-configurations", nil)
+	if err != nil {
+		fmt.Fprintf(w, "Error. %s", err)
+		return
+	}
+	fmt.Printf("kvpair. %v\n", kvpair)
+	if kvpair.Value == nil {
+		fmt.Fprintf(w, "Configuration empty")
+		return
+	}
+	val := string(kvpair.Value)
+	fmt.Fprintf(w, "%s", val)
+
+}
+
+func Products(w http.ResponseWriter, r *http.Request) {
+	products := []product{
+		{
+			ID:    1,
+			Name:  "User 1",
+			Price: 2000000.00,
+		},
+		{
+			ID:    2,
+			Name:  "User 2",
+			Price: 500.00,
+		},
+		{
+			ID:    3,
+			Name:  "User 3",
+			Price: 1500000.00,
+		},
+		{
+			ID:    4,
+			Name:  "User 4",
+			Price: 50000.00,
+		},
+		{
+			ID:    5,
+			Name:  "User 5",
+			Price: 20000000.00,
+		},
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&products)
+}
+
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `product service is good`)
+}
+
+func port() string {
+	p := os.Getenv("PRODUCT_SERVICE_PORT")
+	if len(strings.TrimSpace(p)) == 0 {
+		return ":8100"
+	}
+	return fmt.Sprintf(":%s", p)
+}
+
+func hostname() string {
+	hn, err := os.Hostname()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return hn
+}
