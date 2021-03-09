42a9d982f970c38574a0674a7d713447d64d9e69
up1 <somkiat.p@gmail.com>
2021-03-09 16:21:28 +0700

    Add demo service discovery with docker

---
+++ sync/working-with-service-discovery/user-service-docker/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type User struct {
+	ID       uint64    `json:"id"`
+	Username string    `json:"username"`
+	Products []product `json:"products"`
+}
+
+type product struct {
+	ID    uint64  `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+func main() {
+	http.HandleFunc("/healthcheck", healthcheck)
+	http.HandleFunc("/user-products", UserProduct)
+	fmt.Printf("user service is up on port: %s", port())
+	http.ListenAndServe(port(), nil)
+}
+
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `user service is good`)
+}
+
+func UserProduct(w http.ResponseWriter, r *http.Request) {
+	p := []product{}
+	client := &http.Client{}
+	resp, err := client.Get("http://product-service" + "/products")
+	if err != nil {
+		fmt.Fprintf(w, "Error. %s", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if err := json.NewDecoder(resp.Body).Decode(&p); err != nil {
+		fmt.Fprintf(w, "Error. %s", err)
+		return
+	}
+	u := User{
+		ID:       1,
+		Username: "demo@gmail.com",
+	}
+	u.Products = p
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&u)
+}
+
+func port() string {
+	p := os.Getenv("USER_SERVICE_PORT")
+	if len(strings.TrimSpace(p)) == 0 {
+		return ":8080"
+	}
+	return fmt.Sprintf(":%s", p)
+}
+
+func hostname() string {
+	hn, err := os.Hostname()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return hn
+}
